5f27ff7464a18dcaf1b23fce4b490df6ff1eb8be
Cameron Jack <cameron.jack@anu.edu.au>
2023-01-12 15:25:32 +1100

    * FIXED: skipping first line of Stage3 output. Confused about lack of header
    * FIXED: skipping some result outputs due to change of fields
    
    * CHANGED: Now opens results at the end of matching, so no empty files

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -17,7 +17,7 @@ echo "$VER" | sed '1s/^/1i /' | sed -i -f- changelog.txt
 git add -u
 MSG="$(printf "${COMMENT}")"
 git commit -m "$MSG"
-git tag -a "$VER" -m "$MSG"
+#git tag -a "$VER" -m "$MSG"
 
 git push
 git push origin $VER
