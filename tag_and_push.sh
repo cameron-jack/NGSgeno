c80a4cd0ba69838b8bbfa8ce88ecaf6e510f39ba
Cameron Jack <cameron.jack@anu.edu.au>
2023-01-20 09:14:32 +1100

    added back fixed tag_and_push.sh

---
+++ tag_and_push.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+VER="v0.21.006"
+DATE="Date: $(date)"
+
+read -r -d '' COMMENT << EOM
+<<<<<<< HEAD
+* NEW: Protection against non-ascii characters in reference sequence file
+* NEW: Added checks for open files
+* NEW: Increased reliability and reporting in matching code
+=======
+* NEW: Protection against non-ascii characters in reference sequence file \n
+* NEW: Added checks for open files \n
+* NEW: Increased reliability and reporting in matching code \n
+>>>>>>> be6d0962870e18afd9590c4e767b439874955080
+EOM
+
+mv changelog.txt changelog_old.txt
+echo "$VER" > changelog.txt
+echo "$DATE" >> changelog.txt
+echo "$COMMENT" >> changelog.txt
+echo "" >> changelog.txt
+cat changelog_old.txt >> changelog.txt
+rm changelog_old.txt
+
+#MSG="$(printf "${COMMENT}")"
+#sed -i "$MSG" changelog.txt
+#echo "${COMMENT}" | sed '1s/^/1i /' | sed -i -f- changelog.txt
+#echo "$DATE" | sed '1s/^/1i /' | sed -i -f- changelog.txt
+#echo "$VER" | sed '1s/^/1i /' | sed -i -f- changelog.txt
+
+git add -u
+MSG="$(echo "$COMMENT")"
+git commit --amend -m "$MSG"
+git tag -a "$VER" -m "$MSG"
+
+git push
+git push origin $VER
