e09cc49a1b5b312c6e17507005552abd5b63af31
Cameron Jack <cameron.jack@anu.edu.au>
2023-01-20 08:26:26 +1100

    * NEW: Protection against non-ascii characters in reference sequence file * NEW: Added checks for open files * NEW: Increased reliability and reporting in matching code

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
-# set -x
 
-VER="v0.21.005"
+VER="v0.21.006"
 DATE="Date: $(date)"
 
 read -r -d '' COMMENT << EOM
-* CHANGED: Disabled locking of experiments during testing \n
-
+* NEW: Protection against non-ascii characters in reference sequence file
+* NEW: Added checks for open files
+* NEW: Increased reliability and reporting in matching code
 EOM
 
-#echo "$COMMENT" | sed '1s/^/1i /' | sed -i -f- changelog.txt
+mv changelog.txt changelog_old.txt
+echo "$VER" > changelog.txt
+echo "$DATE" >> changelog.txt
+echo "$COMMENT" >> changelog.txt
+echo "" >> changelog.txt
+cat changelog_old.txt >> changelog.txt
+rm changelog_old.txt
+
+#MSG="$(printf "${COMMENT}")"
+#sed -i "$MSG" changelog.txt
+#echo "${COMMENT}" | sed '1s/^/1i /' | sed -i -f- changelog.txt
 #echo "$DATE" | sed '1s/^/1i /' | sed -i -f- changelog.txt
 #echo "$VER" | sed '1s/^/1i /' | sed -i -f- changelog.txt
 
 git add -u
-MSG="$(printf "${COMMENT}")"
-git commit -m "$MSG"
+MSG="$(echo "$COMMENT")"
+git commit --amend -m "$MSG"
 git tag -a "$VER" -m "$MSG"
 
 git push
