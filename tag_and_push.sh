5d44259101f8580bc75cfba17679da7bacba8c26
Cameron Jack <cameron.jack@anu.edu.au>
2023-10-25 12:29:04 +1100

    * Fixed: bug in replacing R1 in fastq files

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-VER="v0.23.000"
+VER="v0.23.002"
 DATE="Date: $(date)"
 
 read -r -d '' COMMENT << EOM
-* Fixed: clear old reference sequence entries on loading new references
-* Fixed: sets html encoding for Windows plate viewer
-* New: rewrite of matching code for correctness and speed
-* TODO: one bug in matching code, likely of inexact matches
+* Fixed: bug in replacing R1 in fastq files
 EOM
 
 mv changelog.txt changelog_old.txt
@@ -23,5 +20,5 @@ MSG="$(echo "$COMMENT")"
 git commit --amend -m "$MSG"
 git tag -a "$VER" -m "$MSG"
 
-git push
-git push origin $VER
+#git push
+#git push origin $VER
