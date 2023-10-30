e8f48f98754859ce67d8da6d4773b2eb4bbab707
Cameron Jack <cameron.jack@anu.edu.au>
2023-10-30 13:30:52 +1100

    Fixed: removed sample barcodes from filenames in Miseq file

--- bin/generate.py
+++ bin/generate.py
@@ -811,7 +811,7 @@ Adapter,,,,,,,,,,
             hdr = "Sample_ID,Sample_Name,Sample_Plate,Sample_Well,I7_Index_ID,index,I5_Index_ID,index2,Sample_Project,Description".split(',')
             dst = csv.writer(dstfd, dialect='unix', quoting=csv.QUOTE_MINIMAL)
             dst.writerow(hdr)
-            gen = [(util.unguard_pbc(r.pcrPlate, silent=True)+'_'+util.padwell(r.pcrWell), util.unguard(r.sampleBarcode, silent=True), '', '',
+            gen = [(util.unguard_pbc(r.pcrPlate, silent=True)+'_'+util.padwell(r.pcrWell), '', '', '',
                     r.i7name, r.i7bc,r.i5name, r.i5bc,'NGSgeno') for r in s3tab.data]
             
             dst.writerows(gen)
