a32ff631a3e6f78caf566342749afeb3aaad9fa1
Cameron Jack <cameron.jack@anu.edu.au>
2024-02-14 16:18:49 +1100

    * New: variant sequences for each well now saved to variants.fa

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-VER="v0.24.000"
+VER="v0.25.000"
 DATE="Date: $(date)"
 
 read -r -d '' COMMENT << EOM
-* New: amplicon-only runs supported
+* New: variant sequences for each well now saved to variants.fa
 EOM
 
 mv changelog.txt changelog_old.txt
